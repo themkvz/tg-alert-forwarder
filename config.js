e91b4055fdf7ea31990954efa57f42e9e634f00b
themkvz <mukovoz.igor@gmail.com>
2022-05-02 07:47:37 +0300

    init

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
     showOriginalMsg: false, // bool forward original message from source channel
-    alertMsg: 'Повітряна тривога!', // string message
-    clearMsg: 'Відбій повітряної тривоги!', // string message
+    alertMsg: 'тривога!', // string message
+    clearMsg: 'Відбій!', // string message
     alertRegex: '🔴', // regex clear marker
     clearRegex: '🟢', // regex clear marker
-    cityRegex: 'Шепетів[сцк]|Хмельницьк', // regex target city marker
+    cityRegex: '', // regex target city marker
     sourceChannels: ['air_alert_ua'], // array of string channel names
     targetChannel: '', // string channel name
     alertMedia: './media/alert.jpg', // string relative path
